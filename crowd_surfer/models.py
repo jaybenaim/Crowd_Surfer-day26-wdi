f5f744a55bc62cf6292cd2e211931af8d5507d88
jaybenaim <benaimjacob@gmail.com>
2019-08-08 14:15:31 -0400

    added .gitignore

--- crowd_surfer/models.py
+++ crowd_surfer/models.py
@@ -5,3 +5,11 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from datetime import date 
 from django import forms 
+
+
+class Project(models.Model): 
+    title = models.CharField(max_length=255) 
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    funding = models.Float
+class User 

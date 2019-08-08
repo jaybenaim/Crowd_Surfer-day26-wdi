4a69ccccb7d996399edfe19cf8e048359c242a14
jaybenaim <benaimjacob@gmail.com>
2019-08-08 15:01:31 -0400

    added models

--- crowd_surfer/admin.py
+++ crowd_surfer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 from .models import * 
 
+admin.site.register(Project)
+admin.site.register(Reward)
+admin.site.register(Comment)
+admin.site.register(Donation) 
--- crowd_surfer/models.py
+++ crowd_surfer/models.py
@@ -5,11 +5,29 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from datetime import date 
 from django import forms 
-
+from django.contrib.auth.models import User 
 
 class Project(models.Model): 
     title = models.CharField(max_length=255) 
     description = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    funding = models.Float
-class User 
+    funding_start_date = models.DateField() 
+    funding_end_date = models.DateField() 
+
+class Reward(models.Model): 
+    reward_type = models.CharField(max_length=255)
+    reward_item = models.CharField(max_length=255)
+    reward_amount = models.FloatField() 
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) 
+    date = models.DateTimeField(auto_now=True) 
+    text = models.TextField() 
+
+class Donation(models.Model): 
+    amount = models.FloatField() 
+    reward = models.ForeignKey(Reward, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
